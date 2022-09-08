484798eb2de8ba2f4dbcdac5cdec72a890d8ca3a
Silvan Miller <84871231+SilvanMiller@users.noreply.github.com>
2022-09-08 08:42:14 -0300

    Update index.tsx

--- parrot/src/services/config/index.tsx
+++ parrot/src/services/config/index.tsx
@@ -4,8 +4,6 @@ export const baseUrl = axios.create({
     baseURL: "http://localhost:4000/",
 });
 
-export const baseUrlHeroku = axios.create({
-    baseURL: "http://parrott.herokuapp.com/",
-});
+
 
 // export default baseUrl
